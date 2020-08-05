e538eb3ca5aa86c8e666c0bd0895666d3699637b
ravmarcin <39264056+ravmarcin@users.noreply.github.com>
2020-08-05 23:23:25 +0100

    Update DDD_app_run.py

--- .github/DDD_app_run.py
+++ .github/DDD_app_run.py
@@ -1,25 +1,3 @@
-name: DInSAR_Deformation_Detection
-
-on:
-  push:
-    branches: [ master ]
-  pull_request:
-    branches: [ master ]
-
-jobs:
-  build:
-
-    runs-on: windows-latest
-
-    steps:
-    - uses: actions/checkout@v2
-    - name: Set up Python 3.8
-      uses: actions/setup-python@v2
-      with:
-        python-version: 3.8
-    - name: Install dependencies
-      run: |
-        python -m pip install --upgrade pip
-        pip install -r requirements.txt
-        python GUI.py
-
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+python GUI.py
